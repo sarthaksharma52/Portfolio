242f879a517eb7a4363dab2e5767192285737822
Sarthak Sharma <sarthak.dec04@gmail.com>
2024-07-18 03:50:31 +0530

    footer complete icon added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,26 +1,36 @@
-import React from 'react'
-import pdf from '../components/pdf/Sarthak-resume .pdf'
+import React from 'react';
+import pdf from '../components/pdf/Sarthak-resume .pdf';
+import instagramIcon from '../public/assets/instagram.png';
+import githubIcon from '../public/assets/github.png';
+import linkedinIcon from '../public/assets/linkedin.png';
+
 const Footer = () => {
   return (
     <>
-    <div className="footer_main">
-      <div className="container footer">
-      <div className="left footer_items">Sarthak Sharma</div>
-      <div className="mid">
-        <a href="" className='footer_items'>projects</a>
-        <a href="" className='footer_items'>about</a>
-        <a href="" className='footer_items'>contact</a>
-        <a href={pdf} download="Sarthak_Sharma_Resume.pdf" className='footer_items'>resume</a>
+      <div className="footer_main">
+        <div className="container footer">
+          <div className="left footer_items">Sarthak Sharma</div>
+          <div className="mid">
+            <a href="" className='footer_items'>projects</a>
+            <a href="" className='footer_items'>about</a>
+            <a href="" className='footer_items'>contact</a>
+            <a href={pdf} download="Sarthak_Sharma_Resume.pdf" className='footer_items'>resume</a>
+          </div>
+          <div className="right">
+            <a href="https://www.linkedin.com/in/sarthak-sharma-778b28257/" target='_blank' className='footer_items_icon'>
+            <img src={linkedinIcon} alt="linkedin" style={{ width: '24px', height: '24px' }} />
+            </a>
+            <a href="https://www.instagram.com/exe.sarthak/" className='footer_items_icon'>
+              <img src={instagramIcon} alt="Instagram" style={{ width: '24px', height: '24px' }} />
+            </a>
+            <a href="https://github.com/sarthaksharma52" target="_blank" className='footer_items_icon'>
+                <img src={githubIcon} alt="github" style={{ width: '24px', height: '24px' }} />
+                </a>
+          </div>
         </div>
-      <div className="right">
-        
-        <a href="https://www.linkedin.com/in/sarthak-sharma-778b28257/" target='_blank' className='footer_items_icon'>linkedin</a>
-        <a href="" className='footer_items_icon'>insta</a>
-        </div>
-    </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
