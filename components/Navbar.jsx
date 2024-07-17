b81660983cb0ff8491b7d9ef7a1d8e48437d66cf
Sarthak Sharma <sarthak.dec04@gmail.com>
2024-07-18 03:00:02 +0530

    linkedin link update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,7 +9,7 @@ const navbar = () => {
         <a href="" className='nav_items'>projects</a>
         <a href="" className='nav_items'>about</a>
         <a href="" className='nav_items'>contact</a>
-        <a href="" className='nav_items linkedin'>linkedin</a>
+        <a href="https://www.linkedin.com/in/sarthak-sharma-778b28257/" className='nav_items linkedin'target='_blank'>linkedin</a>
         </div>
     </div>
     </div>
