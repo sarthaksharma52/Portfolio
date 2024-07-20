f6f71cbf56c38db32420791740d12effe2af729b
Sarthak Sharma <sarthak.dec04@gmail.com>
2024-07-20 12:59:42 +0530

    update links of card

--- components/Card.jsx
+++ components/Card.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 import './Card1.css';
 
 const Card = ({ title, year, imageSrc, liveLink, sourceCodeLink, cardColor, textColor }) => {
-    return (
+  return (
+    <a href={sourceCodeLink} target="_blank" rel="noopener noreferrer" className="card-link">
       <div className="card" style={{ borderColor: cardColor, backgroundColor: cardColor, color: textColor }}>
         <div className="card-header" style={{ backgroundColor: cardColor }}>
           <div className="card-year" style={{ color: textColor }}>{year}</div>
-          <a href={sourceCodeLink} target="_blank" rel="noopener noreferrer" className="card-arrow" style={{ color: textColor }}>
+          <div className="card-arrow" style={{ color: textColor }}>
             ➔
-          </a>
+          </div>
         </div>
         <div className="card-title" style={{ color: textColor }}>{title}</div>
         <a href={liveLink} target="_blank" rel="noopener noreferrer" className="card-image-link">
           <img src={imageSrc} alt={title} className="card-image" />
         </a>
-        
       </div>
-      
-    );
-  };
+    </a>
+  );
+};
 
 export default Card;
